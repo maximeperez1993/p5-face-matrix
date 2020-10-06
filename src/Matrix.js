065a80516b5a424e98a87ceed14a6e3c6a1339aa
Maxime Perez <mperez@enovacom.fr>
2020-10-06 13:07:12 +0200

    Improve perf

--- src/Matrix.js
+++ src/Matrix.js
@@ -12,12 +12,17 @@ class Matrix {
         this.pixelsMark = [];
 
         this.maxElements = (width / columnProperties.charSize) - 1;
+
+        this.imageRight = (width / 2 - image.width / 2) + image.width;
+        this.imageLeft = (width / 2 - image.width / 2);
+        this.imageBottom = (height / 2 - image.height / 2) + image.height;
+        this.imageTop = (height / 2 - image.height / 2);
     }
 
     paint(image) {
         let x = round(random(this.maxElements)) * columnProperties.charSize;
         let column = this.columns.find(column => column.vector.x === x);
-        if (column == null && random() < frameCount / 2000) {
+        if (column == null && random() < frameCount / 100) {
             this.columns.push(new Column(x, columnProperties));
         }
 
@@ -32,18 +37,20 @@ class Matrix {
     }
 
     fillSet(vector) {
+
+        if (vector.x < this.imageLeft || vector.x >= this.imageRight ||
+            vector.y < this.imageTop || vector.y >= this.imageBottom) return;
+
         let probability = 1 - (sqrt(this.pixels.length + 1) * 2) / 1000;
+
         for (let x = 0; x < charSize; x++) {
             for (let y = 0; y < charSize; y++) {
                 if (random() < probability) continue;
                 let newX = vector.x + x;
                 let newY = vector.y + y;
-                let imageRight = (width / 2 - image.width / 2) + image.width;
-                let imageLeft = (width / 2 - image.width / 2);
-                let imageBottom = (height / 2 - image.height / 2) + image.height;
-                let imageTop = (height / 2 - image.height / 2);
 
-                if (newX < imageLeft || newX >= imageRight || newY < imageTop || newY >= imageBottom) continue;
+                if (newX < this.imageLeft || newX >= this.imageRight ||
+                    newY < this.imageTop || newY >= this.imageBottom) continue;
                 this.addPixel(newX, newY);
             }
         }
@@ -60,9 +67,10 @@ class Matrix {
         image.loadPixels();
         loadPixels();
         for (let item of this.pixels) {
+            let index = (item.x + (item.y * width)) * 4;
+
             let x = item.x - (width / 2 - image.width / 2);
             let y = item.y - (height / 2 - image.height / 2);
-            let index = (item.x + (item.y * width)) * 4;
             let imageIndex = (x + (y * image.width)) * 4;
 
             pixels[index] = image.pixels[imageIndex] / 2;
@@ -71,12 +79,6 @@ class Matrix {
             pixels[index + 3] = image.pixels[imageIndex + 3];
         }
         updatePixels();
-    }
-
-    isOnColImage(x) {
-        let imageRight = (width / 2 - image.width / 2) + image.width;
-        let imageLeft = (width / 2 - image.width / 2);
-        return x >= imageLeft && x < imageRight;
     }
 }
 
